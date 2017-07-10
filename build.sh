5990a2ae16b7c0423c966220c9e73c99160b1e10
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-07-10 09:31:01 +0200

    Debian slim

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ echo ""
 
 if [ $? -eq 0 ]; then
     echo -e "\x1b[1;32mBuild Done. To run it: \e[0m"
-    echo 'docker stop "phing-test-ctn"'
-    echo 'docker rm "phing-test-ctn"'
-    echo 'docker run -d --hostname "phing-test-ctn" --name "phing-test-ctn" inet_phing_test'
-    echo 'docker exec -i -t "phing-test-ctn" /bin/bash'
+    echo 'docker run -it --rm inet_phing_test phing'
 fi
+
+
